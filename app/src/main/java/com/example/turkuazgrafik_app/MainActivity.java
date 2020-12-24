a313ff7820ebbebaa7fca47bcabe7800036bc4a8
İsrafil Çetin <icetin93@gmail.com>
2020-12-24 14:01:49 +0300

    test1

--- app/src/main/java/com/example/turkuazgrafik_app/MainActivity.java
+++ app/src/main/java/com/example/turkuazgrafik_app/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.turkuazgrafik_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.api_but);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+
+
     }
+
+    private void jsonParse() {
+        String url = "";
+    }
+
 }
