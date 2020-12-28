75cf23c7ed3c5d5f6f52709b280154fafd2b493a
israfil çetin <icetin93@gmail.com>
2020-12-28 15:27:15 +0300

    classlar eklendi

---
+++ app/src/main/java/com/example/turkuazgrafik_app/BuHafta.java
@@ -0,0 +1,40 @@
+package com.example.turkuazgrafik_app;
+
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+    public class BuHafta  extends Fragment {
+        private String baslik;
+
+        public static BuHafta newInstance(String baslik) {
+            Bundle args = new Bundle();
+            args.putString("BASLIK", baslik);
+            BuHafta fragment = new BuHafta();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            baslik = getArguments().getString("BASLIK");
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.haftasayfa, container, false);
+            TextView title = view.findViewById(R.id.tvBaslik);
+            title.setText(baslik);
+            return view;
+        }
+    }
---
+++ app/src/main/java/com/example/turkuazgrafik_app/FragmentAdapter.java
@@ -0,0 +1,54 @@
+package com.example.turkuazgrafik_app;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    ArrayList<String> basliklar;
+
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        basliklar = new ArrayList<>();
+        basliklar.add("Bugün");
+        basliklar.add("Bu Hafta");
+        basliklar.add("Toplam");
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0: {
+                return Bugun.newInstance(basliklar.get(position));
+            }
+            case 1: {
+                return BuHafta.newInstance(basliklar.get(position));
+            }
+            case 2: {
+                return Toplam.newInstance(basliklar.get(position));
+            }
+            default: {
+                return Bugun.newInstance(basliklar.get(position));
+            }
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return basliklar.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return basliklar.get(position);
+    }
+}
